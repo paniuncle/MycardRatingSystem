4be6078fe3c021fa7896d90f4cfabca2e3a07271
paniuncle <1173440278@qq.com>
2016-09-20 03:46:50 +0800

    remove fallback
    remove fallback

--- Application/Home/Controller/QueryController.class.php
+++ Application/Home/Controller/QueryController.class.php
@@ -6,12 +6,13 @@ class QueryController extends Controller {
 		$player = I('get.username','','strip_tags');//通过Post方法获取玩家名称
 		//$passw = I('post.password','','strip_tags');//通过Post方法获取玩家密码
 		//$safekey = I('post.ak','','strip_tags');//通过Post方法获取是否合法
-		
-			CheckUser($player);
+			if($player == NULL){
+				error_return('500');
+			}
 			$player_data = QueryData($player); //获取玩家的数据
 			if($player_data == NULL){
 				error_return('404');
-			}
+			}else{
 				$EndResult = array(
 						'exp'=>$player_data['exp'],
 						'pt'=>$player_data['pt'],
@@ -21,8 +22,6 @@ class QueryController extends Controller {
 						)
 					);
 			$this->ajaxReturn($EndResult);
-		
-
-		
+			}	
 		}
 }
